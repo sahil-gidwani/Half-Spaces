fae069411bc90d0614392691f3a60206d19578f9
Pranav Mohan <101455049+pranavm28@users.noreply.github.com>
2025-03-27 15:35:03 +0530

    Add files via upload

--- halfspaces_app.py
+++ halfspaces_app.py
@@ -174,7 +174,7 @@ def process_halfspace_data(data_passes, data_carries, mins_data):
     # Merge with mins_data
     try:
         combined_prog_df = pd.merge(combined_prog_df, 
-                                    mins_data[['player', 'team', '90s', 'position']], 
+                                    mins_data[['player', 'team', '90s', 'league']], 
                                     on=['player', 'team'], how='left')
     except Exception as e:
         st.error(f"Error merging with minutes data: {e}")
