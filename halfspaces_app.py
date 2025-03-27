d8856eb7b02d6ba806cefdfe874fded3bd4d510b
Pranav Mohan <101455049+pranavm28@users.noreply.github.com>
2025-03-27 16:24:40 +0530

    Add files via upload

--- halfspaces_app.py
+++ halfspaces_app.py
@@ -177,8 +177,8 @@ def process_halfspace_data(data_passes, data_carries, mins_data):
         mins_data['player'] = mins_data['player'].str.strip().str.upper()
 
 # Clean team names as well for good measure
-        #combined_prog_df['team'] = combined_prog_df['team'].str.strip().str.upper()
-        #mins_data['team'] = mins_data['team'].str.strip().str.upper()
+        combined_prog_df['team'] = combined_prog_df['team'].str.strip().str.upper()
+        mins_data['team'] = mins_data['team'].str.strip().str.upper()
 
 # Then perform the merge
         combined_prog_df = pd.merge(combined_prog_df, 
